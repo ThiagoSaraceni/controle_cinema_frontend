3cbc950d93160b12ee9610c31cd7ba684ef88e50
ThiagoSaraceni <thiagorx123@gmail.com>
2024-11-07 23:34:09 -0300

    first commit

---
+++ controle_cinema_frontend/src/App.tsx
@@ -0,0 +1,18 @@
+import GlobalStyles from "./globalStyles";
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <div>
+        <h4>
+          minha primeira aplicacao
+          dasifdjgsdaoflksadgkofsdkoghsfdokgoksadokgsdaokoksdakfodsokfdaskofdasok
+        </h4>
+        <h4>mentira</h4>
+      </div>
+    </>
+  );
+}
+
+export default App;
