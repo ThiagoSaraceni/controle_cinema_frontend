441aac3c416c818f3fd8ec28bd85f447fcc3f3f9
ThiagoSaraceni <thiagorx123@gmail.com>
2024-11-25 23:29:32 -0300

    feat: estilo geral background movie

---
+++ controle_cinema_frontend/src/ pages/cinema/styles.jsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Background = styled.div`
+  padding: 92px 200px 92px 200px;
+  color: white;
+  min-height: 100vh;
+  background-image: url("Frame 60.jpg");
+`;
