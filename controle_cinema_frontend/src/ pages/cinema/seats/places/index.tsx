25a5dde89b516c14a7911980c1ac390eb716bcc4
ThiagoSaraceni <thiagorx123@gmail.com>
2025-01-22 18:23:31 -0300

    feat: logica assentos

--- controle_cinema_frontend/src/ pages/cinema/seats/places/index.tsx
+++ controle_cinema_frontend/src/ pages/cinema/seats/places/index.tsx
@@ -1,4 +1,6 @@
+import { useEffect, useState } from "react";
 import { ChairIcon } from "../../../../assets/svg/cinema/chair-icon";
+import { UserBlockSvg } from "../../../../assets/svg/cinema/user-icon";
 import * as S from "../seatsInfo/styles";
 
 type SeatStatus = 0 | 1 | 2; // 0: Disponível, 1: Selecionado, 2: Reservado
@@ -7,6 +9,8 @@ export const SeatPlaces = () => {
   const initialSeats: SeatStatus[][] = Array(8)
     .fill(0)
     .map(() => Array(10).fill(0)); // Matriz 8x11 com assentos disponíveis
+
+  const [seats, setSeats] = useState<SeatStatus[][]>(initialSeats);
 
   const fileiras = [
     "a",
@@ -28,28 +32,47 @@ export const SeatPlaces = () => {
     "q",
   ];
 
+  console.log(seats);
+
+  const toggleSeat = (row: any, col: any) => {
+    if (seats[row][col] === 2) {
+      return;
+    }
+    const newSeats = [...seats];
+    newSeats[row][col] = seats[row][col] === 0 ? 1 : 0;
+    setSeats(newSeats);
+  };
+
+  const seatsMarked: any = [];
+  seats.forEach((array, rowIndex) => {
+    array.forEach((valueArray, colIndex) => {
+      if (valueArray === 1) {
+        seatsMarked.push({
+          fileira: fileiras[seats.length - (rowIndex + 1)],
+          coluna: colIndex + 1,
+        });
+      }
+    });
+  });
+
+  console.log(seatsMarked);
+
+  //logica existe reservad ou n, se n ta reservado ao clicar vc pega o valor atual e seta e algum e muda, mas ja se o valor é igual a 1 mostrar o tipo ocuapdo
   return (
     <>
       <S.CenterContent>
         <div>
           <S.DisplayFlexHeader>
             <div className="space" />
-            {initialSeats[0].map((_, colIndex) => (
+            {seats[0].map((_, colIndex) => (
               <div className="sit">
                 <p>{colIndex + 1}</p>
               </div>
             ))}
             <p />
           </S.DisplayFlexHeader>
-          {initialSeats.map((array, rowIndex) => {
+          {seats.map((array, rowIndex) => {
             /*aqui dou um map no meus 8 arrays, e o rowIndex é o count do array atual, que vai de 0 a 7*/
-            const row = rowIndex;
-
-            console.log({
-              initialSeats: initialSeats.length,
-              rowIndex,
-              math: initialSeats.length - (rowIndex + 1),
-            });
 
             return (
               <>
@@ -64,19 +87,45 @@ export const SeatPlaces = () => {
                           <p>
                             {
                               /*Explicação pego o tamanho do meu array principal, e faço sempre um menos valor da minha linha atual, assim fazendo a soma decrescente. do maior para o menor. */
-                              fileiras[initialSeats.length - (rowIndex + 1)]
+                              fileiras[seats.length - (rowIndex + 1)]
                             }
                           </p>
                         )}
-                        <S.Avatar onClick={() => console.log(`${row}${col}`)}>
-                          <ChairIcon />
-                        </S.Avatar>
+                        {valueArray === 0 ? (
+                          <S.Avatar
+                            onClick={() => {
+                              toggleSeat(rowIndex, colIndex);
+                            }}
+                            /* onClick={() =>
+                              console.log(
+                                `${
+                                  fileiras[seats.length - (rowIndex + 1)]
+                                }${col}`
+                              )
+                            }*/
+                          >
+                            <ChairIcon />
+                          </S.Avatar>
+                        ) : valueArray === 1 ? (
+                          <S.Avatar
+                            bgColor="#D0FF00"
+                            onClick={() => {
+                              toggleSeat(rowIndex, colIndex);
+                            }}
+                          >
+                            {fileiras[seats.length - (rowIndex + 1)]}
+                            {col}
+                          </S.Avatar>
+                        ) : (
+                          <S.Avatar bgColor="#EA5455">
+                            <UserBlockSvg />
+                          </S.Avatar>
+                        )}
+
                         {
                           /*Faço isso para escrever o valor apos o ultimo valor do meu array */
                           colIndex === array.length - 1 && (
-                            <p>
-                              {fileiras[initialSeats.length - (rowIndex + 1)]}
-                            </p>
+                            <p>{fileiras[seats.length - (rowIndex + 1)]}</p>
                           )
                         }
                       </>
