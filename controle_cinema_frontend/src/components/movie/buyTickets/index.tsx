383608a240428496573332a2efc72f526d3c263c
ThiagoSaraceni <thiagorx123@gmail.com>
2025-01-26 15:05:32 -0300

    feat: pegando tipo de ingresso  e quantidade de cada ingresso.

--- controle_cinema_frontend/src/components/movie/buyTickets/index.tsx
+++ controle_cinema_frontend/src/components/movie/buyTickets/index.tsx
@@ -1,13 +1,37 @@
+import { useState } from "react";
 import { Card } from "../../card";
 import { CardBody } from "../../cardBody";
 import { Ticket } from "./ticket";
+import { useSelector } from "react-redux";
+import { RootState } from "../../../redux";
 
 export const BuyTickets = () => {
+  const { seatsMarked } = useSelector((state: RootState) => state.movie);
+  const qntSeats = seatsMarked?.length;
+  const [fullPrice, setFullPrice] = useState(0);
+  const [halfPrice, setHalfPrice] = useState(0);
+  const sumTickets = halfPrice + fullPrice;
+
+  const cantAdd = qntSeats === sumTickets;
+
   return (
     <Card>
       <CardBody>
-        <Ticket title="INTEIRA" value="24.00" />
-        <Ticket margin title="MEIA" value="12.00" />
+        <Ticket
+          title="INTEIRA"
+          value="24.00"
+          currentValue={fullPrice}
+          setValue={setFullPrice}
+          cantAdd={cantAdd}
+        />
+        <Ticket
+          margin
+          title="MEIA"
+          value="12.00"
+          currentValue={halfPrice}
+          setValue={setHalfPrice}
+          cantAdd={cantAdd}
+        />
       </CardBody>
     </Card>
   );
